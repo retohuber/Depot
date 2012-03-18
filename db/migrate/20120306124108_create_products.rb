f12b9ccd1e7356c17f2bc6037008634f01c61194
Reto Huber <huberre1@students.zhaw.ch>
2012-03-18 15:14:59 +0100

    kapitel 6

--- db/migrate/20120306124108_create_products.rb
+++ db/migrate/20120306124108_create_products.rb
@@ -4,9 +4,12 @@ class CreateProducts < ActiveRecord::Migration
       t.string :title
       t.text :description
       t.string :image_url
-      t.decimal :price
+      t.decimal :price, :precision => 8, :scale => 2
 
       t.timestamps
     end
+  end
+  def self.down
+    drop_table :products
   end
 end
