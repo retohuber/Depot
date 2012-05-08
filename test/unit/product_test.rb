7ad5809cf0d6bfb14e3a69caa5beb841d841c634
Reto Huber <huberre1@students.zhaw.ch>
2012-05-08 14:00:48 +0200

    letzte zwei KApitel

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,15 @@
-require 'test_helper'
-
-class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+test "product price must be positive" do
+product = Product.new(:title => "My Book Title",
+:description => "yyy",
+:image_url => "zzz.jpg")
+product.price = -1
+assert product.invalid?
+assert_equal "must be greater than or equal to 0.01",
+product.errors[:price].join('; ')
+product.price = 0
+assert product.invalid?
+assert_equal "must be greater than or equal to 0.01",
+product.errors[:price].join('; ')
+product.price = 1
+assert product.valid?
 end
